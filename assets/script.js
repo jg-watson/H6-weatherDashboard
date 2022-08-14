2425c5f785c25ef24ffd840a105f9dfb107f6b99
naps&snacks <j.antoniogarcia1130@gmail.com>
2022-08-13 18:07:39 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-console.log(hello);
+//Declared variables 
+
+var searchButton = $("search-button");
+var searchCity = $("search-city");
+
+//Api Key 
+var APIKey="dff049b28f04d7e6c1fe41fa2dd60317";
+
+function forcast(cityid) {
+    var queryForecastURL= "https://api.openweathermap.org/data/2.5/weather?id="+ cityid + "&appid=" + APIKey;
+}
+
