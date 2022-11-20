a87dfafaf9629962210bf531f7ed24095708665b
naps&snacks <j.antoniogarcia1130@gmail.com>
2022-11-19 20:46:17 -0600

    updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # H6-weatherDashboard
+
+## Description
+A five day weather dashboard that runs in the browser and features dynamically updated HTML and CSS. 
+
+## Installation
+Required api key from OpenWeather API.
+
+## Usage
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,31 @@
 //Declared variables 
-
+var currentDateEl =$("date");
 var searchButton = $("search-button");
 var searchCity = $("search-city");
-
+var currentForecast = ('c-forecast');
 //Api Key 
 var APIKey="dff049b28f04d7e6c1fe41fa2dd60317";
 
-function forcast(cityid) {
-    var queryForecastURL= "https://api.openweathermap.org/data/2.5/weather?id="+ cityid + "&appid=" + APIKey;
+function forecast(cityid) {
+    var forecastURL= "https://api.openweathermap.org/data/2.5/weather?id="+ cityid + "&appid=" + APIKey;
+
+function fetchWeather(location) 
+    var {lon} = longitude;
+    var {lat} = latitude;
+    var{timezone} = timezone;
+    var {current} = currentWeather;
+
+    fetch(forecastUrl)
+    .then(function(res) {
+    return res.json();
+    })
+    .catch (function (err) {
+        console.error(err);
+    });
 }
 
+
+
+
+
+fetchButton.addEventListener('click',getApi);
